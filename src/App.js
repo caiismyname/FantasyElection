e71ab1efcc6f6a26ce7fde2236e7c8149685dbe9
caiismyname <davidcai2012@gmail.com>
2020-02-04 22:20:46 -0800

    basic firebase hooked up

--- src/App.js
+++ src/App.js
@@ -1,38 +1,49 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
+import {apiKey, authDomain, databaseURL, projectId, storageBucket, appId, measurementId} from "./firebaseCredentials";
+const Firebase = require('firebase/app');
+require('firebase/database');
+const uuidv4 = require('uuid/v4');
 
-class OnboardingPage extends React.Component {
+class FirebaseManager {
+  constructor() {
 
-
-  render() {
-    return (
-      <div>
-        <h1>Welcome to Fantasy Elections: Democratic Primaries 2020</h1>
-        <PlayerEntry />
-      </div>
-    );
+    const firebaseConfig = {
+        apiKey: apiKey,
+        authDomain: authDomain,
+        databaseURL: databaseURL,
+        projectId: projectId,
+        storageBucket: storageBucket,
+        appId: appId,
+        measurementId: measurementId,
+      };
+    
+      Firebase.initializeApp(firebaseConfig);
   }
+
+  createNewGame(players) {
+    const id = uuidv4();
+    Firebase.database().ref(id + "/players").set({...players});
+  }
+
 }
 
-class PlayerEntry extends React.Component {
+
+class OnboardingPage extends React.Component {
   constructor(props) {
     super(props);
 
     this.addNewPlayer = this.addNewPlayer.bind(this);
+    this.changePlayerName = this.changePlayerName.bind(this);
+    this.createNewGame = this.createNewGame.bind(this);
 
     this.state = {
       players: {
         0: "",
         1: "",
-      }
+      },
+      firebaseManager: new FirebaseManager(),
     }
-  }
-
-  textChangeHandler(idx, value) {
-    const newPlayers = {...this.state.players};
-    newPlayers[idx] = value;
-    this.setState({players: newPlayers});
   }
 
   addNewPlayer() {
@@ -42,15 +53,48 @@ class PlayerEntry extends React.Component {
     this.setState({players: newPlayers});
   }
 
+  changePlayerName(idx, value) {
+    const newPlayers = {...this.state.players};
+    newPlayers[idx] = value;
+    this.setState({players: newPlayers});
+  }
+
+  createNewGame() {
+    this.state.firebaseManager.createNewGame(this.state.players);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Fantasy Elections: Democratic Primaries 2020</h1>
+        <PlayerEntry 
+          players={this.state.players}
+          addNewPlayerHandler={() => this.addNewPlayer()}
+          changePlayerNameHandler={(idx, value) => this.changePlayerName(idx, value)}
+        />
+        <button 
+          onClick={this.createNewGame}
+          style={{margin:"200px 0 0"}}
+        >Submit</button>
+      </div>
+    );
+  }
+}
+
+class PlayerEntry extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     const playerFields = [];
 
-    for (let playerIdx in this.state.players) {
+    for (let playerIdx in this.props.players) {
       const field = 
         <textarea 
           style={{border: "1px solid black", width:"100%"}}
-          value={this.state.players[playerIdx]}
-          onChange={event => {this.textChangeHandler(playerIdx, event.target.value)}}
+          value={this.props.players[playerIdx]}
+          onChange={event => {this.props.changePlayerNameHandler(playerIdx, event.target.value)}}
           key={playerIdx}
         />;
       playerFields.push(field);
@@ -60,7 +104,7 @@ class PlayerEntry extends React.Component {
       <div style={{width: "50%", margin: "0 auto"}}>
         {playerFields}
         <div 
-          onClick={this.addNewPlayer} 
+          onClick={this.props.addNewPlayerHandler} 
         >
           Add new Player
         </div>
@@ -68,9 +112,6 @@ class PlayerEntry extends React.Component {
     );
   }
 }
-
-
-
 
 function App() {
   return (
